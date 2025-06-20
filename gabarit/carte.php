83caa44930df225650159bcc8173f7cef5a0dbf7
Danilo Costa <dancc86@gmail.com>
2025-06-20 10:01:09 -0400

    ajouter des fonctions sur le fichier carte.php pour ajouter les temperatures

--- gabarit/carte.php
+++ gabarit/carte.php
@@ -7,5 +7,10 @@ $lien = " <a href=" . get_permalink() . ">Suite</a>";
     <div class="conteneur__carte-text">
         <h2><?php the_title(); ?></h2>
         <p><?php echo wp_trim_words(get_the_excerpt(), 35, $lien); ?></p>
+        <div>
+            <small>Temp Max: <i class="conteneur__temp-haut"><?php the_field('temperature_max'); ?>&deg;C</i></small>
+            <small>Temp Moyenne: <i class="conteneur__temp-moy"><?php the_field('temperature_moyenne'); ?>&deg;C</i></small>
+            <small>Temp Min: <i class="conteneur__temp-min"><?php the_field('temperature_min'); ?>&deg;C</i></small>
+        </div>
     </div>
 </article>
