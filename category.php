bd79fdb11d5ab52f72d3df9553a9a4ef06de68b0
Danilo Costa <dancc86@gmail.com>
2025-06-22 15:08:40 -0400

    enlever certains commentaires du code

--- category.php
+++ category.php
@@ -3,48 +3,6 @@
 <!-- <h1> pour debogade - a retirer ---------- category.php -----------</h1> -->
 
 
-<!-- <section class="formulaire">
-  <form action="" class="formulaire__contenu">
-    <div>
-      <label for="nom">Nom</label>
-      <input
-        class="formulaire__input"
-        type="text"
-        name="nom"
-        id="nom"
-        placeholder="Ecrivez votre nom" />
-    </div>
-    <div>
-      <label for="prenom">Prenom</label>
-      <input
-        class="formulaire__input"
-        type="text"
-        name="prenom"
-        id="prenom"
-        placeholder="Ecrivez votre Prenom" />
-    </div>
-    <div>
-      <label for="telephone">Telephone</label>
-      <input
-        class="formulaire__input"
-        type="text"
-        name="telephone"
-        id="telephone"
-        placeholder="Ecrivez votre telephone" />
-    </div>
-    <div>
-      <label for="nom">Courriel</label>
-      <input
-        class="formulaire__input"
-        type="text"
-        name="curiel"
-        id="courriel"
-        placeholder="Ecrivez votre courriel" />
-    </div>
-    <button type="submit" class="hero__lien">S'inscrire</button>
-  </form>
-</section> -->
-
 <section class="populaire">
   <h2><?php single_cat_title(); ?></h2>
   <?php echo category_description(); ?>
