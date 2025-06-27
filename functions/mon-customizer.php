28bc0885e7aafd1e9d749464e534f0f3659d8f85
Danilo Costa <dancc86@gmail.com>
2025-06-27 09:39:08 -0400

    ajouter la fonction pour afficher l'option hero dans le site WP

--- functions/mon-customizer.php
+++ functions/mon-customizer.php
@@ -1,7 +1,22 @@
-<?php function theme_31w_customize_register($wp_customize)
+<?php function theme_33w_customize_register($wp_customize)
 // configuration des nouveau panneaux du customizer
 {
     // Le code pour ajouter des sections, des réglages et des contrôles ira ici.
+    $wp_customize->add_section('hero_section', array(
+        'title' => __('Section Hero - Accueil', 'theme_33w'),
+        'priority' => 30,
+    ));
+
+    $wp_customize->add_setting('hero_title', array(
+        'default' => __('Bienvenue sur mon site', 'theme_33w'),
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+
+    $wp_customize->add_control('hero_title', array(
+        'label' => __('Hero Title', 'theme_33w'),
+        'section' => 'hero_section',
+        'type' => 'text',
+    ));
 }
 
-add_action('customize_register', 'theme_31w_customize_register');
+add_action('customize_register', 'theme_33w_customize_register');
